fc12e07b9726c339d7c72ef07f5bce01538d450f
pain-arch <himulol123@gmail.com>
2024-05-20 01:05:00 +0600

    feat: Add orange surface to the game window

--- code/main.py
+++ code/main.py
@@ -4,8 +4,13 @@ import pygame
 pygame.init()
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-
+pygame.display.set_caption('Space Fighter Game') # Set the window title
 running = True
+
+# surface
+surf = pygame.Surface((100, 200))
+surf.fill('orange')
+x = 100 # x position of the surface
 
 while running:
     # Handle events
@@ -14,6 +19,11 @@ while running:
             running = False
             
     # Draw the game
+    display_surface.fill('gray') # Fill the screen with red
+    x += 1 # Move the surface to the right
+    display_surface.blit(surf, (x, 150))
+    pygame.display.update() # Update the display
+    
 
 # Quit the game
 pygame.quit() 
