d1594aad1792d13ce84e523d832590051ec1eaec
pain-arch <himulol123@gmail.com>
2024-05-20 03:10:07 +0600

    Refactor game rendering logic for improved performance

--- code/main.py
+++ code/main.py
@@ -15,10 +15,13 @@ surf.fill('orange')
 x = 100 # x position of the surface
 
 
-# importing an image
+# imports the player and star images
 player_surf = pygame.image.load(join('images', 'player.png')).convert_alpha()
+player_rect = player_surf.get_frect(topleft = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+
 star_surf = pygame.image.load(join('images', 'star.png')).convert_alpha()
 star_positions = [(randint(0, WINDOW_WIDTH), randint(0 , WINDOW_HEIGHT)) for _ in range(20)] # Generate random positions for the stars
+
 
 while running:
     # Handle events
@@ -30,8 +33,8 @@ while running:
     display_surface.fill('darkgray') # BG color 
     for pos in star_positions:       # Draw the stars on the screen 
       display_surface.blit(star_surf, pos )# Draw the surface
-    x += 0.5 # Move the surface to the right
-    display_surface.blit(player_surf, (x, 150))
+        # Move t
+    display_surface.blit(player_surf, player_rect) # Draw the player on the screen
     
    
     
